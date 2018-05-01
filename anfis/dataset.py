8979826db53e745dc54a46bae6bdded25980c9b3
Damar Pramuditya <pramudityad@yahoo.com>
2018-05-02 04:56:28 +0700

    change dataset

--- anfis/dataset.py
+++ anfis/dataset.py
@@ -1,10 +1,18 @@
-import itertools
 import csv
+import itertools
 temperature = [30,30,30,30]
 humidity = [10,30,60,90]
-soil = [120,240,360,480]
-rain = [60,120,240,360]
+soil = [60,120,240,360]
+rain = [6,12,24,36]
 
 dataset = open("dataset.csv","w")
-wr = csv.writer(dataset ,quoting=csv.QUOTE_ALL)
-wr.writerows(list(itertools.product(temperature,humidity,soil,rain)))
+with open('dataset.csv',newline='') as f:
+    r = csv.reader(f)
+    wr = csv.writer(dataset ,quoting=csv.QUOTE_ALL)
+    data = [line for line in r]
+
+with open('dataset.csv','w',newline='') as f:
+    wr = csv.writer(f)
+    wr.writerow(['temperature','humidity','soil','rain'])
+    wr.writerows(list(itertools.product(temperature,humidity,soil,rain)))
+    wr.writerows(data)
