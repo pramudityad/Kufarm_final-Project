638f96f663c4a31bc8610b20a0985c84744fbffb
pramudityad <pramudityad@yahoo.com>
2018-05-03 19:25:29 +0700

    modified:   lab_app.py

--- Pi-logger/lab_app/lab_app.py
+++ Pi-logger/lab_app/lab_app.py
@@ -24,9 +24,9 @@ def read_temp():
 	import Adafruit_DHT
 	humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 4)
 	mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(0, 0))
-	soil = mcp.read_adc(5)
-	rain = mcp.read_adc(6)
-	soil, rain = 1024-soil
+	val = 1024
+	soil = mcp.read_adc(5)-val
+	rain = mcp.read_adc(6)-val
 	if humidity is not None and temperature is not None:
 		return render_template("lab_temp.html",soil=soil,rain=rain,temp=temperature,hum=humidity)
 	else:
