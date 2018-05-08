5fd54fee39b54dd245e37de7a86b764642a2fc40
Damar Pramuditya <pramudityad@yahoo.com>
2018-05-08 20:25:24 +0700

    add app.py

--- Pi-logger/lab_app/app.py
+++ Pi-logger/lab_app/app.py
@@ -75,8 +75,8 @@ def lab_env_db():
 
 def get_records():
 	import MySQLdb
-	from_date_str 	= request.args.get('from',time.strftime("%Y-%m-%d 00:00")) #Get the from date value from the URL
-	to_date_str 	= request.args.get('to',time.strftime("%Y-%m-%d %H:%M"))   #Get the to date value from the URL
+	from_date_str 	= request.args.get('from',time.strftime("%Y-%m-%d 00:00:00")) #Get the from date value from the URL
+	to_date_str 	= request.args.get('to',time.strftime("%Y-%m-%d %H:%M:%S:%S"))   #Get the to date value from the URL
 	timezone 		= request.args.get('timezone','Etc/UTC');
 	range_h_form	= request.args.get('range_h','');  #This will return a string, if field range_h exists in the request
 	range_h_int 	= "nan"  #initialise this variable with not a number
@@ -93,35 +93,35 @@ def get_records():
 	print "Received from browser: %s, %s, %s, %s" % (from_date_str, to_date_str, timezone, range_h_int)
 	
 	if not validate_date(from_date_str):			# Validate date before sending it to the DB
-		from_date_str 	= time.strftime("%Y-%m-%d 00:00")
+		from_date_str 	= time.strftime("%Y-%m-%d 00:00:00")
 	if not validate_date(to_date_str):
-		to_date_str 	= time.strftime("%Y-%m-%d %H:%M")		# Validate date before sending it to the DB
+		to_date_str 	= time.strftime("%Y-%m-%d %H:%M:%S")		# Validate date before sending it to the DB
 	print '2. From: %s, to: %s, timezone: %s' % (from_date_str,to_date_str,timezone)
 	# Create datetime object so that we can convert to UTC from the browser's local time
-	from_date_obj       = datetime.datetime.strptime(from_date_str,'%Y-%m-%d %H:%M')
-	to_date_obj         = datetime.datetime.strptime(to_date_str,'%Y-%m-%d %H:%M')
+	from_date_obj       = datetime.datetime.strptime(from_date_str,'%Y-%m-%d %H:%M:%S')
+	to_date_obj         = datetime.datetime.strptime(to_date_str,'%Y-%m-%d %H:%M:%S')
 
 	# If range_h is defined, we don't need the from and to times
 	if isinstance(range_h_int,int):	
 		arrow_time_from = arrow.utcnow().replace(hours=-range_h_int)
 		arrow_time_to   = arrow.utcnow()
-		from_date_utc   = arrow_time_from.strftime("%Y-%m-%d %H:%M")	
-		to_date_utc     = arrow_time_to.strftime("%Y-%m-%d %H:%M")
-		from_date_str   = arrow_time_from.to(timezone).strftime("%Y-%m-%d %H:%M")
-		to_date_str	    = arrow_time_to.to(timezone).strftime("%Y-%m-%d %H:%M")
+		from_date_utc   = arrow_time_from.strftime("%Y-%m-%d %H:%M:%S")	
+		to_date_utc     = arrow_time_to.strftime("%Y-%m-%d %H:%M:%S")
+		from_date_str   = arrow_time_from.to(timezone).strftime("%Y-%m-%d %H:%M:%S")
+		to_date_str	    = arrow_time_to.to(timezone).strftime("%Y-%m-%d %H:%M:%S")
 	else:
 		#Convert datetimes to UTC so we can retrieve the appropriate records from the database
-		from_date_utc   = arrow.get(from_date_obj, timezone).to('Etc/UTC').strftime("%Y-%m-%d %H:%M")	
-		to_date_utc     = arrow.get(to_date_obj, timezone).to('Etc/UTC').strftime("%Y-%m-%d %H:%M")
+		from_date_utc   = arrow.get(from_date_obj, timezone).to('Etc/UTC').strftime("%Y-%m-%d %H:%M:%S")	
+		to_date_utc     = arrow.get(to_date_obj, timezone).to('Etc/UTC').strftime("%Y-%m-%d %H:%M:%S")
 
 	conn 			    = MySQLdb.connect(host="localhost",
 					 user="logger",
 					 passwd="password",
 					 db="gfarm")
 	curs 			    = conn.cursor()
-	curs.execute("SELECT * FROM dht11 WHERE rDateTime BETWEEN ? AND ?", (from_date_utc.format('YYYY-MM-DD HH:mm'), to_date_utc.format('YYYY-MM-DD HH:mm')))
+	curs.execute("SELECT temperature FROM dht11 WHERE created_at BETWEEN ? AND ?", (from_date_utc.format('YYYY-MM-DD HH:mm'), to_date_utc.format('YYYY-MM-DD HH:mm')))
 	temperatures 	    = curs.fetchall()
-	curs.execute("SELECT * FROM dht11 WHERE rDateTime BETWEEN ? AND ?", (from_date_utc.format('YYYY-MM-DD HH:mm'), to_date_utc.format('YYYY-MM-DD HH:mm')))
+	curs.execute("SELECT humidity FROM dht11 WHERE created_at BETWEEN ? AND ?", (from_date_utc.format('YYYY-MM-DD HH:mm'), to_date_utc.format('YYYY-MM-DD HH:mm')))
 	humidities 		    = curs.fetchall()
 	conn.close()
 
@@ -192,7 +192,7 @@ def to_plotly():
 
 def validate_date(d):
 	try:
-		datetime.datetime.strptime(d, '%Y-%m-%d %H:%M')
+		datetime.datetime.strptime(d, '%Y-%m-%d %H:%M:%S')
 		return True
 	except ValueError:
 		return False
