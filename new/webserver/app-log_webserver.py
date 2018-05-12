1bb5a68436158e31c621292c92fcd78753a0b828
Damar Pramuditya <pramudityad@yahoo.com>
2018-05-12 11:41:29 +0700

    modify main

--- new/webserver/app-log_webserver.py
+++ new/webserver/app-log_webserver.py
@@ -7,6 +7,7 @@ import sqlite3
 import Adafruit_DHT
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
+import log_sensor as sensor
 
 from flask import Flask, render_template, send_file, make_response, request
 app = Flask(__name__)
@@ -16,57 +17,6 @@ conn=sqlite3.connect('../kufarm.db')
 curs=conn.cursor()
 
 sampleFreq = 1*300 # time in seconds ==> Sample each 5 min
-
-# get data from DHT sensor
-def getdht():	
-	Sensor = Adafruit_DHT.DHT11
-	DHTpin = 4
-	hum, temp = Adafruit_DHT.read_retry(Sensor, DHTpin)
-	if hum is not None and temp is not None:
-		hum = round(hum)
-		temp = round(temp, 1)
-	return temp, hum
-
-# get data from spi sensor
-def getsoil():
-	SPI_PORT   = 0
-	SPI_DEVICE = 0
-	mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
-	soil = mcp.read_adc(5)
-	soil = 1024-soil
-	return soil
-
-def getrain():
-	SPI_PORT   = 0
-	SPI_DEVICE = 0
-	mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
-	rain = mcp.read_adc(6)
-	rain = 1024-rain
-	return rain
-
-# log dht sensor data on database
-def logdht (temp, hum):
-	myTime  	= datetime.datetime.now()
-	currentTime	= myTime.strftime('%Y-%m-%d %H:%M:%S')
-	curs.execute("INSERT INTO DHT_data (timestamp, temp, hum) values('"+currentTime+"', (?), (?))", (temp, hum))
-	conn.commit()
-	conn.close()
-
-# log spi sensor data on database
-def logsoil (soil):
-	myTime  	= datetime.datetime.now()
-	currentTime	= myTime.strftime('%Y-%m-%d %H:%M:%S')
-	curs.execute("INSERT INTO soil (timestamp, value) values('"+currentTime+"', "+str(soil)+")")
-	conn.commit()
-	conn.close()
-
-# log spi sensor data on database
-def lograin (rain):
-	myTime  	= datetime.datetime.now()
-	currentTime	= myTime.strftime('%Y-%m-%d %H:%M:%S')
-	curs.execute("INSERT INTO rain (timestamp, value) values('"+currentTime+"', "+str(rain)+")")
-	conn.commit()
-	conn.close()
 
 # Retrieve LAST data from database
 def getLastData():
@@ -142,12 +92,12 @@ rangeTime = 100
 @app.route("/")
 def index():
 	while True:
-		temp, hum = getdht()
-		soil = getsoil()
-		rain = getrain()
-		logdht (temp, hum)
-		logsoil (soil)
-		lograin (rain)
+		temp, hum = sensor.getdht()
+		soil = sensor.getsoil()
+		rain = sensor.getrain()
+		sensor.logdht (temp, hum)
+		sensor.logsoil (soil)
+		sensor.lograin (rain)
 		time.sleep(sampleFreq)
 	time, temp, hum, soil, rain = getLastData()
 	templateData = {
--- new/webserver/log_sensor.py
+++ new/webserver/log_sensor.py
@@ -4,7 +4,7 @@ import Adafruit_DHT
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 dbname='kufarm.db'
-sampleFreq = 1*300 # time in seconds ==> Sample each 5 min
+#sampleFreq = 1*300 # time in seconds ==> Sample each 5 min
 
 # get data from DHT sensor
 def getdht():	
