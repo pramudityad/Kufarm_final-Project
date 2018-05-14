e7380b859dd3e0b571b2a97e3b04649829ca95b6
Damar Pramuditya <pramudityad@yahoo.com>
2018-05-14 12:49:26 +0700

    modify input_data.py

--- new/input_data.py
+++ new/input_data.py
@@ -307,17 +307,46 @@ def lograin (rain):
 # main function
 def main():
 	print "Start"
-	while (requestStatus == False):
-		requestData()
-		time.sleep(1)
-		temp, hum = getdht()
-		soil = getsoil()
-		rain = getrain()
-		logdht (temp, hum)
-		logsoil (soil)
-		lograin (rain)
-		time.sleep(sampleFreq)
-	cekOwCode();
-	cekWuCode();
+	while True:
+			now = datetime.datetime.now()
+			timeRequest = now.strftime('%Y-%m-%d %H:%M:%S');
+			terbit = hisab.terbit(DB.getTimezone(),DB.getLatitude(),DB.getLongitude(),0)
+			terbenam = hisab.terbenam(DB.getTimezone(),DB.getLatitude(),DB.getLongitude(),0)
+			strTerbit   = str(int(math.floor(terbit)))+":"+str(int((terbit%1)*60))
+			strTerbenam = str(int(math.floor(terbenam)))+":"+str(int((terbenam%1)*60))
+			print timeRequest
+			if(now.hour%1==0 and now.minute%30.0==0 and now.second==0):
+				requestData()
+				cekOwCode()
+				cekWuCode()
+				#DB.addSoil(soil);
+				#DB.addRaindrop(rain);
+				#soil = DB.getLastSoil();
+				if(now.minute==0 and now.second==0):
+					timeRequest = now.strftime('%Y-%m-%d %H:00:00');
+					if(now.hour == 0):
+							DB.addSunTime([strTerbit,strTerbenam])
+					code = WU.getForcastByTime(str_wu_data, str(now.hour))['fctcode']
+					weather = WU.getForcastByTime(str_wu_data, str(now.hour))['condition']
+					wsp = "wunderground"
+					DB.addForecast(code,weather,wsp,timeRequest)
+					if(now.hour%3==0):
+						code = OW.getForcastByTime(str_ow_data, timeRequest)['weather'][0]['id']
+						weather = OW.getForcastByTime(str_ow_data, timeRequest)['weather'][0]['description']
+						wsp = "openweather"
+						DB.addForecast(code,weather,wsp,timeRequest)
+			try:
+				#soil = SPI.readSensor(0)
+				#rain = SPI.readSensor(1)
+				temp, hum = getdht()
+			soil = getsoil()
+			rain = getrain()
+			logdht (temp, hum)
+			logsoil (soil)
+			lograin (rain)
+			time.sleep(sampleFreq)
+			except Exception as e:
+				print e
+		
 # ------------ Execute program 
 main()
