0eb13a538b46c53ba586942fb1fe467948b68309
Damar Pramuditya <pramudityad@yahoo.com>
2018-05-19 16:50:24 +0700

    indentity

--- new/inputv2.py
+++ new/inputv2.py
@@ -292,8 +292,8 @@ def getdht():
 	hum, temp = Adafruit_DHT.read_retry(Sensor, DHTpin)
 	if hum is not None and temp is not None:
 		'Temperature={0:0.1f}C'.format(temp, hum)
-	    	pass
-	    'Humidity={1:0.1f}%'.format(temp, hum)
+			pass
+		'Humidity={1:0.1f}%'.format(temp, hum)
 		#hum = round.format(hum)
 		#temp = round.format(temp, 1)
 	return temp, hum
