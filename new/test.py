6c8835bbb265bb89e28ea0825774f51c69c740c3
Damar Pramuditya <pramudityad@yahoo.com>
2018-05-20 06:08:04 +0700

    modify main

--- new/test.py
+++ new/test.py
@@ -3,11 +3,11 @@ import time
 sampleFreq = 60
 
 #def main():
-	while True:
-		temp, hum = DT.getdht()
-		pass
-		DT.logdht(temp, hum)
-		time.sleep(sampleFreq)
+	#while True:
+		#temp, hum = DT.getdht()
+		#pass
+		#DT.logdht(temp, hum)
+		#time.sleep(sampleFreq)
 
 # ------------ Execute program 
 #main()
