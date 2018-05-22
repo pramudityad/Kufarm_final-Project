e24b6e2d14c5ca92ed473965b2307ca283b2e712
Damar Pramuditya <pramudityad@yahoo.com>
2018-05-22 15:59:13 +0700

    modify log_sensor

--- new/log_sensor.py
+++ new/log_sensor.py
@@ -12,10 +12,11 @@ def getdht():
 	DHTpin = 4
 	hum, temp = Adafruit_DHT.read_retry(Sensor, DHTpin)
 	if hum is not None and temp is not None:
-		hum = round(hum)
-		temp = round(temp, 1)
-	except Exception as e:
-	    raise e
+		try:
+			hum = round(hum)
+			temp = round(temp, 1)
+		except Exception as e:
+			raise e
 	return temp, hum
 
 # get data from spi sensor
