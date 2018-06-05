97ba6a95fd144170ee5a19228325071951675c24
Damar Pramuditya <pramudityad@yahoo.com>
2018-06-06 04:02:02 +0700

    edit test

--- new/test.py
+++ new/test.py
@@ -1,14 +1,20 @@
 # "test"
 #import fuzzy_v2 as fuzzy
 import database_sqlite as DB
-import RPi.GPIO as GPIO
-import time
+#import RPi.GPIO as GPIO
+import time, datetime
+import hisab as hisab
+import fuzzy as fuzzy
+import math
 
 pinwatering     = 18
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+""" GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM) """
 
-def init_output(pinwatering):
+terbit = hisab.terbit(DB.getTimezone(),DB.getLatitude(),DB.getLongitude(),0)
+terbenam = hisab.terbenam(DB.getTimezone(),DB.getLatitude(),DB.getLongitude(),0)
+
+""" def init_output(pinwatering):
 	GPIO.setup(pinwatering, GPIO.OUT)
 	GPIO.output(pinwatering, GPIO.LOW)
 	GPIO.output(pinwatering, GPIO.HIGH)
@@ -21,6 +27,24 @@ def pump_on():
     GPIO.output(pinwatering, GPIO.HIGH)
     GPIO.cleanup()
     DB.addPumpLog('watering pump','OFF')
+ """
+def main():
+    soil = 100
+    rain = 150
+    temp = 30
+    hum = 70
+    ow_code = 0
+    now = datetime.datetime.now()
+    timeRequest = now.strftime('%Y-%m-%d %H:%M:%S');
+    terbit = hisab.terbit(DB.getTimezone(),DB.getLatitude(),DB.getLongitude(),0)
+    strTerbit   = str(int(math.floor(terbit)))+":"+str(int((terbit%1)*60))
+    strTerbenam = str(int(math.floor(terbenam)))+":"+str(int((terbenam%1)*60))
+    
+    #NK = fuzzy.calculate(soil,rain,temp,hum,ow_code)
+    if((math.floor(terbit) == now.hour and int((terbit%1)*60) == now.minute)):
+        NK = fuzzy.calculate(soil,rain,temp,hum,ow_code)
+        if(NK>65):
+            print(NK)
 
-pump_on()
-
+if __name__ == '__main__':
+    main()
