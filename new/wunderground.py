4ed54ef4f0911be914e0e2f14ae0915c4b09affb
Damar Pramuditya <pramudityad@yahoo.com>
2018-06-23 19:41:21 +0700

    edit wunderground

--- new/wunderground.py
+++ new/wunderground.py
@@ -3,15 +3,23 @@ import json
 
 # Look up POP in near Future 
 def getpop(a):
+	pop = '0'
 	url    = 'http://api.wunderground.com/api/003508f51f58d4f4/geolookup/forecast/q/-6.978887,107.630328.json'
 	result = urllib.request.urlopen(url).read()
 	data   = json.loads(result.decode('utf-8'))
-	pop    =  data['forecast']['txt_forecast']['forecastday'][a]['pop']
+	try:
+		pop = data['forecast']['txt_forecast']['forecastday'][a]['pop']
+	except HTTPError as e:
+		pop = "{\"status\":\"error\"}"
 	return pop
 
 def getweather(a):
+	condition = 'sunny'
 	url    = 'http://api.wunderground.com/api/003508f51f58d4f4/geolookup/forecast/q/-6.978887,107.630328.json'
 	result = urllib.request.urlopen(url).read()
 	data   = json.loads(result.decode('utf-8'))
-	condition = data['forecast']['txt_forecast']['forecastday'][a]['icon']
+	try:
+		condition = data['forecast']['txt_forecast']['forecastday'][a]['icon']
+	except HTTPError as e:
+		condition = "{\"status\":\"error\"}"
 	return condition
