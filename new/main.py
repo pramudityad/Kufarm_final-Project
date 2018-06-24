a78f2d780e4ea3cf505a51fcc0879ff68ca37f1f
Damar Pramuditya <pramudityad@yahoo.com>
2018-06-24 13:08:54 +0700

    edit main

--- new/main.py
+++ new/main.py
@@ -45,6 +45,7 @@ maxtimewatering = 1;
 treshold 		= 300;
 
 
+
 ow_hujan_code   = {500,501,502,503,504,511,520,521,522,531,300,301,302,310,311,312,313,314,321}
 ow_mendung_code = {803,804}
 ow_cerah_code   = {800,801,802}
