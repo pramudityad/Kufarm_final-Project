86b91193e7a92a67140f5d4070e47072461165c0
pramudityad <pramudityad@yahoo.com>
2018-06-26 08:28:20 +0700

    modified:   kufarm.db 	modified:   main.py

--- new/main.py
+++ new/main.py
@@ -42,7 +42,7 @@ timePupuk     = 0;
 overrideSiram = False;
 delaySecond   = 1;
 maxtimewatering = 1;
-treshold 		= 290;
+treshold 		= 200;
 
 ow_hujan_code   = {500,501,502,503,504,511,520,521,522,531,300,301,302,310,311,312,313,314,321}
 ow_mendung_code = {803,804}
