f552b37a68030b4d9affeec761edb5544c9662f5
Damar Pramuditya <pramudityad@yahoo.com>
2018-06-29 15:49:38 +0700

    edit file

--- new/plot_acf.py
+++
@@ -1,6 +0,0 @@
-from pandas import Series
-from matplotlib import pyplot
-from statsmodels.graphics.tsaplots import plot_acf
-series = Series.from_csv("C:\\Users\\Damar Pramuditya\\Desktop\\soil_test3.csv", header=0)
-plot_acf(series)
-pyplot.show()
