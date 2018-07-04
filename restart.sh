24d4460085a953ac2905aa0b6155a1ff9c6acd10
Damar Pramuditya <pramudityad@yahoo.com>
2018-07-04 18:26:29 +0700

    edit restart

--- restart.sh
+++ restart.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 #cari semua PID
-<<<<<<< HEAD
 $pid=`ps ax | grep 'main2.py'`
-=======
-$pid=`ps aux | grep 'main2.py'`
->>>>>>> 7057002ba4b5548ad8d193ab91814ee3f78e1e49
 
 #jika PID tidak ditemukan maka print empty, jika isi maka di kill
 if [ -z $pid ]
@@ -21,8 +17,4 @@ sleep 5
 printf '%s\tStart Application\n' "$(date +'%T %A %d %B %Y')" >>/home/pi/Damar/forecast/logs/restartlog
 cd /home/pi/Damar/forecast/new
 rm -f /home/pi/Damar/forecast/new/nohub.out
-<<<<<<< HEAD
 nohup python3 /home/pi/Damar/forecast/new/main2.py & python3 /home/pi/Damar/forecast/new/control_panel.py && fg
-=======
-nohup python3 /home/pi/Damar/forecast/new/main2.py & python3 /home/pi/Damar/forecast/new/control_panel.py && fg
->>>>>>> 7057002ba4b5548ad8d193ab91814ee3f78e1e49
