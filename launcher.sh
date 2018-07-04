053442cd6c142970a27f991fa8c423ca24a7151a
pramudityad <pramudityad@yahoo.com>
2018-07-04 17:13:08 +0700

    modified:   launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 printf '%s\tStart Application\n' "$(date +'%T %A %d %B %Y')" 
-sleep 3
-cd /
+sleep 5
 cd /home/pi/Damar/forecast/new
-sudo python3 main.py
+sudo python3 main2.py & python3 control_panel.py && fg
+#sudo nohup python3 main2.py 
 cd /
