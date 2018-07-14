3f04316348272e21612e1b97cd614f3d5df64c61
pramudityad <pramudityad@yahoo.com>
2018-07-14 11:38:14 +0700

    modified:   new/database_sqlite.py

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 printf '%s\tStart Application\n' "$(date +'%T %A %d %B %Y')" 
-sleep 5
+sleep 3
 cd /home/pi/Damar/forecast/new
 sudo python3 main2.py & python3 control_panel.py && fg
 #sudo nohup python3 main2.py 
