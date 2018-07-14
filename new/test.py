d4e7ccde57b3175e039be22dc21c79846d354d4f
Damar Pramuditya <pramudityad@yahoo.com>
2018-07-14 11:35:38 +0700

    edit test

--- new/test.py
+++ new/test.py
@@ -1,4 +1,4 @@
-import schedule
+#import schedule
 import time, datetime
 import slot as SL
 import database_sqlite as DB
@@ -11,23 +11,7 @@ def job():
 	t0 = time.time()
 	return t0
 
+temp = 42
+hum = 45
 
-soil = 300
-wu_desc = 'Sunny'
-command = 'watering'
-
-def main():
-	global t0
-	global ts
-	t0 = job()
-	schedule.every(ts).minutes.do(job)
-	while True:
-		schedule.run_pending()
-		t1 = t0 + (ts*1)*60
-		print ("========================")
-		print ("check circumstances every	: "+str(ts)+" minute")
-		print ("check again 		: " +time.strftime("%I %M %p",time.localtime(t1)))
-		time.sleep(1)
-
-if __name__ == '__main__':
-	main()
+x = SL.adv_decision(temp,hum,)
