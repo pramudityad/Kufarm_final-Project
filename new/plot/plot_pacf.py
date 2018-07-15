537a1d2ef9babfbfb0d9ffd1fc14483ef75d316d
Damar Pramuditya <pramudityad@yahoo.com>
2018-07-15 14:14:11 +0700

    edit pacf

--- new/plot/plot_pacf.py
+++ new/plot/plot_pacf.py
@@ -1,6 +1,6 @@
 from pandas import Series
 from matplotlib import pyplot
 from statsmodels.graphics.tsaplots import plot_pacf
-series = Series.from_csv("C:\\Users\\Damar Pramuditya\\Desktop\\soil_test3.csv", header=0)
+series = Series.from_csv("D:\\Univ\\Semester 8\\TA 2\\Source Code\\forecast\\new\plot\\soil_test3.csv", header=0)
 plot_pacf(series, lags=50)
 pyplot.show()
